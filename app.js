678d16963d9f7d50b8e37f192a67e979fcf479e7
Leo GERBERON <leo.gerberon@equasens.com>
2022-10-28 13:25:05 +0200

    add startup lookup

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 const {spawn} = require('node:child_process');
-const {mkdir} = require('node:fs/promises');
+const fs = require('fs/promises');
 
+let state = {
+    currentIndex: 0,
+    uri: process.env.URI || '',
+    intervalleImage: process.env.INTERVAL || 10000,
+    recordTime: process.env.RECORD_TIME || 3600000,
+    videoName: process.env.VIDEO_NAME || 'defaultName'
+}
 
-let uri = process.env.URI || '';
-let intervalleImage = process.env.INTERVAL || 10000;
-let recordTime = process.env.RECORD_TIME || 3600000;
-let videoName = process.env.VIDEO_NAME || 3600000;
+const path = state.videoName + '/state.json';
 
-let currentIndex = 0
-
-console.log(`Enregistrement d'un timelapse de ${recordTime} tout les ${intervalleImage}`)
+console.log(`Enregistrement d'un timelapse de ${state.recordTime} tout les ${state.intervalleImage}`)
 
 function registerImage() {
-    const ffmpeg = spawn('ffmpeg', ['-i', uri, '-ss', '00:00:01', '-f', 'image2', '-vframes', '1',
-        `${videoName}/image-${currentIndex}.jpg`, '-v', 'error'])
+    const ffmpeg = spawn('ffmpeg', ['-i', state.uri, '-ss', '00:00:01', '-f', 'image2', '-vframes', '1',
+        `${state.videoName}/image-${state.currentIndex}.jpg`, '-v', 'error'])
     ffmpeg.on('close', (code) => {
-        console.log(`Image ${currentIndex} created with code : ${code}`);
+        console.log(`Image ${state.currentIndex} created with code : ${code}`);
         if (code === 0) {
-            currentIndex++
+            state.currentIndex++
+            fs.writeFile(path, JSON.stringify(state))
         }
     })
     ffmpeg.stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
     });
-
 }
 
 
 function createVideo() {
-    const ffmpeg = spawn('ffmpeg', ['-i', `${videoName}/image-%d.jpg`, '-vcodec', 'mpeg4', `${videoName}.avi`])
+    const ffmpeg = spawn('ffmpeg', ['-i', `${state.videoName}/image-%d.jpg`, '-vcodec', 'mpeg4', `${state.videoName}.avi`])
     ffmpeg.on('close', (code) => {
-        console.log(`Video créer ${currentIndex} created with code : ${code}`);
+        console.log(`Video créer ${state.currentIndex} created with code : ${code}`);
         process.exit(0);
     })
     ffmpeg.stderr.on('data', (data) => {
@@ -39,10 +41,31 @@ function createVideo() {
 
 }
 
-mkdir(videoName).then(() => console.log(`Dossier ${videoName} créer`))
-    .catch((e) => console.log(`Erreur durant la creation du dossier`, e))
-    .finally(() => {
-        setInterval(registerImage, intervalleImage);
-        setTimeout(createVideo, recordTime)
+
+function startRecord() {
+    setInterval(registerImage, state.intervalleImage);
+    setTimeout(createVideo, state.recordTime)
+}
+
+fs.access(state.videoName)
+    .then(() => {
+        console.log("Dossier Timelapse trouvé")
+        return fs.access(path);
     })
+    .then(() => {
+        return fs.readFile(path)
+    })
+    .then((readedStateBuffer) => {
+        let readedState = JSON.parse(readedStateBuffer)
+        console.log("Reprise du state ", readedState)
+        state = readedState;
+    })
+    .catch(err => {
+        console.log("Auncun timelapse en cours creation du dossier et du state ")
+        fs.writeFile(path, JSON.stringify(state))
+    })
+    .then(() => {
+        startRecord();
+    })
+
 
